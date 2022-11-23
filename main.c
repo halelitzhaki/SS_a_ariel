9489b9da6e4fdd716306de87677f1a2d530d38b5
Halel Itzhaki <halel840@gmail.com>
2022-11-23 14:24:41 +0000

    eighth commit

--- main.c
+++ main.c
@@ -7,38 +7,38 @@ int main()
     scanf("%d", &num2);
 
     //is Armstrong
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     for(int i=num1; i<=num2; i++)
     {
         if(isArmstrong(i)==1)
-            printf("%d ", i);
+            printf(" %d", i);
     }
     printf("\n");
 
     //is palindrome
-    printf("The Palindromes are: ");
+    printf("The Palindromes are:");
     for(int i=num1; i<=num2; i++)
     {
         if(isPalindrome(i)==1)
-            printf("%d ", i);
+            printf(" %d", i);
     }
     printf("\n");
 
     //is prime
-    printf("The Prime numbers are: ");
+    printf("The Prime numbers are:");
     for(int i=num1; i<=num2; i++)
     {
         if(isPrime(i)==1)
-            printf("%d ", i);
+            printf(" %d", i);
     }
     printf("\n");
 
     //is strong
-    printf("The Strong numbers are: ");
+    printf("The Strong numbers are:");
     for(int i=num1; i<=num2; i++)
     {
         if(isStrong(i)==1)
-            printf("%d ", i);
+            printf(" %d", i);
     }
     printf("\n");
 
